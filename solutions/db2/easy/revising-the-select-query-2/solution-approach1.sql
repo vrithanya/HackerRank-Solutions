a5a563235c3d44121a4045d78f28ac1451dda290
vrithanya <vrithanya08@gmail.com>
2026-09-23 22:58:33 +0530

    ✅ Revising the Select Query II [db2] — approach1

---
+++ solutions/db2/easy/revising-the-select-query-2/solution-approach1.sql
@@ -0,0 +1,22 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true
+-- Problem     Revising the Select Query II
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-09-23, 10:58 p.m.
+-- ──────────────────────────────────────────────────
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+SELECT name
+FROM city
+WHERE Countrycode ='USA'
+AND Population >120000 ; 
