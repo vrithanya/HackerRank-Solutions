0f25a40e1a8a15373e4a1e983f596fe8e74c6903
vrithanya <vrithanya08@gmail.com>
2026-09-24 22:12:59 +0530

    ✅ Select All [db2] — approach1

---
+++ solutions/db2/easy/select-all-sql/solution-approach1.sql
@@ -0,0 +1,13 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=true
+-- Problem     Select All
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    db2
+-- Status      Accepted
+-- Submitted   2026-09-24, 10:12 p.m.
+-- ──────────────────────────────────────────────────
+
+
+SELECT * FROM CITY ;
