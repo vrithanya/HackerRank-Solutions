5c1928b147944edfd74d72a94921b17ba7d76f57
vrithanya <vrithanya08@gmail.com>
2026-09-24 21:57:57 +0530

    ✅ Java Output Formatting [java] — approach1

---
+++ solutions/java/easy/java-output-formatting/solution-approach1.java
@@ -0,0 +1,31 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-output-formatting/problem?isFullScreen=true
+// Problem     Java Output Formatting
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-24, 09:57 p.m.
+// ──────────────────────────────────────────────────
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                
+            System.out.printf("%-15s%03d%n",s1,x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
